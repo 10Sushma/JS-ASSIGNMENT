74e587be011778e4e8914eb9c7d716b3e38ff30c
10Sushma <10.sushmakumar@gmail.com>
2022-03-05 12:35:56 +0530

    Create_object

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,17 @@
 function CreateObject(arr) {
     // Write your code here
+    const arr1=[]
+    const arr2=[]
+    for (i=0;i<arr.length;i++){
+        if(i%2==0){
+            arr1.push(arr[i])
+        }
+        else{
+            arr2.push(arr[i])
+        }
+    }
+    var res=Object.assign.apply({},arr1.map((v,i)=>({[v]:arr2[i]})));
+    return res
 }
 
 module.exports = CreateObject;
